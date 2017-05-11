59a87f1bc468b40cb689bc98044cc1f3963b9139
Franco Bulgarelli <franco@mumuki.org>
2017-05-10 21:22:28 -0300

    Simplest test_hook

--- template/lib/test_hook.rb
+++ template/lib/test_hook.rb
@@ -1,25 +1,14 @@
 class <CONSTANT>TestHook < Mumukit::Templates::FileHook
   mashup
   isolated true
-  structured true
 
   def tempfile_extension
-    '.rb'
+    '...'
   end
 
   def command_line(filename)
-    "rspec #{filename} -f json"
-  end
-
-  def to_structured_result(result)
-    transform(super['examples'])
-  end
-
-  def transform(examples)
-    examples.map { |e| [e['full_description'], e['status'].to_sym, parse_out(e['exception'])] }
-  end
-
-  def parse_out(exception)
-    exception ? exception['message'] : ''
+    "... #{filename}"
   end
 end
+
+
