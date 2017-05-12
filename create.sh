152fe83e30020a7ea43fd472216375be2c0533db
Federico Scarpa <fedescarpa@users.noreply.github.com>
2017-05-12 11:11:44 -0300

    fixing `https` typo

--- create.sh
+++ create.sh
@@ -111,6 +111,6 @@ git commit -m "Inital commit"
 echo "[Mumukit::Bootstrap] Done!"
 echo "                     Now go to $project_directory and start editing it."
 echo ""
-echo "                     See htts://github.com/mumuki/mumukit-bootstrap/README.md#Development for more instructions"
+echo "                     See https://github.com/mumuki/mumukit-bootstrap/README.md#Development for more instructions"
 
 
